98830e985c956a49bf28ffc3ff28c2dc6c349e05
knightofcode <knightofcode@users.noreply.github.com>
2020-05-18 05:04:14 +0000

    update giphy-anywhere to v0.3.3

--- Casks/giphy-anywhere.rb
+++ Casks/giphy-anywhere.rb
@@ -1,6 +1,6 @@
 cask 'giphy-anywhere' do
-  version 'master'
-  sha256 ''
+  version '0.3.3'
+  sha256 '8b1cecf2c46eb77d05b42a7c2c87d067d46439db6ef71545a3cfe0f27fc80176'
 
   url "https://github.com/bacongravy/giphy-anywhere/releases/download/v#{version}/GIPHY_Anywhere.dmg"
   appcast 'https://github.com/bacongravy/giphy-anywhere/releases.atom'
