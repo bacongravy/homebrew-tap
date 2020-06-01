fddbe2a106d5c3f576062a481632015aab326a53
David Kramer <davidkramer@me.com>
2020-06-01 15:14:43 -0700

    fix mac-app-devops-demo url

--- Casks/mac-app-devops-demo.rb
+++ Casks/mac-app-devops-demo.rb
@@ -2,7 +2,7 @@ cask 'mac-app-devops-demo' do
   version '1.0.0'
   sha256 '38da4af1c1274a1929191ea6c0014f43c4bb8c5ec090857bdd81375400d2f042'
 
-  url "https://github.com/bacongravy/mac-app-devops-demo/releases/download/v#{version}/mac-app-devops-demo.dmg"
+  url "https://github.com/bacongravy/mac-app-devops-demo/releases/download/v#{version}/mac_app_devops_demo.dmg"
   appcast 'https://github.com/bacongravy/mac-app-devops-demo/releases.atom'
   name 'mac-app-devops-demo'
   homepage 'https://github.com/bacongravy/mac-app-devops-demo'
