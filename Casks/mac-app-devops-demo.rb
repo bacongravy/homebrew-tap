4bde3400a862850622e047a55f0e8371988defef
bacongravy <bacongravy@users.noreply.github.com>
2020-06-01 22:12:44 +0000

    update mac-app-devops-demo to v1.0.0

--- Casks/mac-app-devops-demo.rb
+++ Casks/mac-app-devops-demo.rb
@@ -1,6 +1,6 @@
 cask 'mac-app-devops-demo' do
-  version ''
-  sha256 ''
+  version '1.0.0'
+  sha256 '38da4af1c1274a1929191ea6c0014f43c4bb8c5ec090857bdd81375400d2f042'
 
   url "https://github.com/bacongravy/mac-app-devops-demo/releases/download/v#{version}/mac-app-devops-demo.dmg"
   appcast 'https://github.com/bacongravy/mac-app-devops-demo/releases.atom'
