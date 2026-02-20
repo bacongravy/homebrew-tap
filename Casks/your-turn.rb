58ccb09c3fdb7d037d56ab4124c7d7b42f4c0b99
David Kramer <bacongravy@icloud.com>
2026-02-20 00:11:30 -0800

    Update your-turn to 1.1.1

--- Casks/your-turn.rb
+++ Casks/your-turn.rb
@@ -1,11 +1,13 @@
 cask "your-turn" do
-  version "1.1.0"
-  sha256 "daf884677aa9359992f205aac87766d6c4dc9b5fc0a57f661ca43b26830d238c"
+  version "1.1.1"
+  sha256 "0f06dbdf2f27a6d01c75d0919043f366ffff4f1e518b312155b38431484db801"
 
-  url "https://github.com/bacongravy/your-turn/releases/download/v1.1.0/Your.Turn-v1.1.0.zip"
+  url "https://github.com/bacongravy/your-turn/releases/download/v1.1.1/Your-Turn-v1.1.1.zip"
   name "Your Turn"
   desc "Menu bar app that notifies you when Claude Code needs attention"
   homepage "https://github.com/bacongravy/your-turn"
+
+  license "MIT"
 
   app "Your Turn.app"
 end
